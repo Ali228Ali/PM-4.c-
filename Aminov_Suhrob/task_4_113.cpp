2bfee7881a2aae01f5320f9b2d1460b0c2111ef8
Ali228Ali <suhrobaminov2008@gmail.com>
2026-09-24 20:51:28 +0500

    Добавлено решение задачи 4.113

---
+++ Aminov_Suhrob/task_4_113.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream> 
+#include <clocale>
+#include <cmath>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+        int n, m;
+
+    cout << "Введите день n и иесяц m";
+    cin >> n >> m;
+
+    if (n > 0 && n <= 30 && m>0 && m <=12 ) {
+
+    
+
+        cout <<"Предыдущий день" <<  n - 1 << "." << m << endl;
+        cout << "Следующий день"  << n + 1 << "." << m;
+    }
+  
+
+
+  
+
+        return 0;
+    
+
+
+}
